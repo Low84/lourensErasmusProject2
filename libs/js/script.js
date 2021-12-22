75280edd0d8ad021b0962acbd1ff7ca5bc2b7f41
Lourens Erasmus <63657914+Low84@users.noreply.github.com>
2021-12-22 16:22:34 +0000

    Update script.js

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,77 +1,357 @@
-<?php
+// Populate employee table and initialize data tables
+$(document).ready(function () {
+  
+  let data;
+  let locData;
+  let deptData;
+  let editId;
+ 
+  // Searchbar
+  // $('.test').DataTable();
+  // $('[data-toggle="tooltip"]').tooltip();
+ 
+  var myModal = new bootstrap.Modal(document.getElementById('editEmployeeModal'), {
+    keyboard: false,
+  });
+  var myModalAdd = new bootstrap.Modal(document.getElementById('addEmployeeModal'), {
+    keyboard: false,
+  });
+  var myModalDelete = new bootstrap.Modal(document.getElementById('deleteEmployeeModal'), {
+    keyboard: false,
+  });
+ 
+  getAll();
+  getLocations();
+  getDepartments();
+  locationById();
+ 
+  
+  
+  function getAll() {
+  $.ajax({
+    url: "libs/php/getAll.php",
+    type: 'GET',
+    success: function (result) {
+      // console.log(result)
+      data = null;
+      $.each(result.data, function (index, value) {
+        data += "<tr data-personnel-id='" + value.id + "'><td data-title='id'>" + value.id + "</td><td data-title='First Name'>" + value.firstName + "</td><td data-title=Last Name'>" + value.lastName + "</td><td data-title='Location'>" + value.location + "</td><td data-title='Email'>" + value.email + "</td><td data-title='Department'>" + value.department + "</td>";
+        // console.log(value.location);
+        data += "\
+                  <td data-title='Edit/Delete'><a href='#editEmployeeModal' class='edit' data-bs-toggle='modal' data-bs-target='#editEmployeeModal'><i class='far fa-edit'\
+                  data-toggle='tooltip' title='Edit'></i></a>\
+                  <a href='#' class='delete'><i class='far fa-trash-alt'\
+                  data-toggle='tooltip' title='Delete'></i></a></td></tr>";     
+                  
+                  
+      })
+      
+      $('#user_data').html(data);
+ 
+      $(".delete").click(function(){
+        console.log('delete');
+        console.log($(this).closest('tr').data('personnel-id'));
+          Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              $.ajax({
+                type: "POST",
+                url: "libs/php/deleteEmployee.php",
+                data: { 'id': $(this).closest('tr').data('personnel-id') },
+                success: function (data) {
+                  console.log(data);
+                  Swal.fire(
+                    'Deleted!',
+                    'The personnel file has been deleted.',
+                    'success'
+                  )   
+                  getAll(1.5);
+                  //myModalDelete.toggle();   
+                },
+                error: function(jqXHR){
+                  console.log(jqXHR);
+                }      
+              });
+            }
+          })
+        
+      });
+ 
+      $('a[data-bs-target="#editEmployeeModal"]').click(function() {
+ 
+        editId = $(this).closest('tr').data('personnel-id');
+        // console.log(editId);
+ 
+        // Edit new employee modal -- department field populated
+          $.ajax({
+            url: "libs/php/getPersonnelByID.php",
+            type: 'GET',
+            data: {
+              id: editId,
+            },
+            success: function (result) {
+              console.log(result);
+              // let departmentID = result.data.personnel[0]['departmentID'];
+              // console.log(result);
+              // console.log(result.data.personnel[0]['firstName']);
+              // console.log(result.data.personnel[0]['departmentID']);
+              $('#idEdit').attr("value", result.data.personnel[0]['id']);
+              $('#firstNameEdit').attr("value", result.data.personnel[0]['firstName']);
+              $('#lastNameEdit').attr("value", result.data.personnel[0]['lastName']);
+              $('#jobEdit').attr("value", result.data.personnel[0]['jobTitle']);
+              $('#emailEdit').attr("value", result.data.personnel[0]['email']);
+              $("#departmentEdit").val(result.data.personnel[0].departmentID).change();
+              
+            },
+            error: function (jqXHR) {
+              console.log(jqXHR);
+            }
+          })
+ 
+          
+        })
+    },
+    error: function (jqXHR) {
+      console.log(jqXHR);
+    }
+  })
+}
 
-	// example use from browser
-	// http://localhost/companydirectory/libs/php/getDepartmentByID.php?id=<id>
+function getLocations() {
+  $.ajax({
+    url: "libs/php/getAllLocations.php",
+    type: 'GET',
+    success: function (result) {
+      console.log(result);
+      $.each(result.data, function (index, value) {
+        console.log(value.id);
+        locData += '<tr><td id="locationID" data-title="ID">' + value.id + '</td><td data-title="Location">' + value.name + '</td><td>NoD</td><td>' + 'NoP' + '</td>';
+        locData += "\
+                  <td data-title='Edit/Delete'><a href='#editLocationModal' class='edit' data-bs-toggle='modal'><i class='far fa-edit'\
+                  data-toggle='tooltip' title='Edit'></i></a>\
+                  <a href='#deleteEmployeeModal' class='delete' data-bs-toggle='modal'><i class='far fa-trash-alt'\
+                  data-toggle='tooltip' title='Delete'></i></a></td></tr>";
+                  $('#location').append($("<option />").val(value.id).text(value.name));
+                  $('#locationDept').append($("<option />").val(value.id).text(value.name));                  
+                  $('#addNewLocation').append($("<option />").val(value.id).text(value.name));
 
-	// remove next two lines for production	
+      })
+      $('#locationData').html(locData);
+      
+    },
+    error: function (jqXHR) {
+      console.log(jqXHR);
+    }
+  })
+}
+ 
+// function locationById(data) {
+//   $.ajax({
+//     url: "libs/php/getLocationByID.php",
+//     type: 'GET',
+//     data: {
+//       id: data
+//     },
+//     success: function (result) {
+  
+//       console.log(result);
+//       return result;
+//       // $.each(result.data, function (index, value) {
+//       //   console.log(value.name);
+//       //   $('#deptLocation').html(value.name);
+//       // })    
+//     },
+//     error: function (jqXHR) {
+//       console.log(jqXHR);
+//     }
+//   })
+// }
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+async function locationById(data) {
+  const result = await $.ajax({
+  url: "libs/php/getLocationByID.php",
+  type: 'GET',
+  data: {
+    id: data
+  }
+});
+console.log(result);
+return result;
+}
 
-	$executionStartTime = microtime(true);
 
-	include("config.php");
+function getDepartments() {
+  $.ajax({
+    url: "libs/php/getAllDepartments.php",
+    type: 'GET',
+    success: function (result) {
+      console.log(result);
+      $.each(result.data, function (index, value) {
+        // console.log(value.id);
+        // console.log(value.locationID);
+        locationById(value.locationId).then(function(data){console.log(data)});
+        var locationName;
+        locationById(value.locationId).then(function(data){locationName = data["data"][0]["name"]});
 
-	header('Content-Type: application/json; charset=UTF-8');
+        
+        deptData += `<tr><td data-title="ID" id="departmentTableId" data-department-id="${value.id}">${value.id}</td><td data-title="Department">${value.name}</td><td id="deptLocation "data-title="Depts. Location"></td><td id="numPersonnel" data-title="No Of Depts"></td>`;
+        deptData += "\
+                  <td data-title='Edit/Delete'><a href='#editDepartModal' class='edit' data-bs-toggle='modal' data-bs-target='#editDepartModal'><i class='far fa-edit'\
+                  data-toggle='tooltip' title='Edit'></i></a>\
+                  <a href='#' class='delete' data-bs-toggle='modal'><i class='far fa-trash-alt'\
+                  data-toggle='tooltip' title='Delete'></i></a></td></tr>";
+                  
+                  $('#addNewDepartment').append(`<option value="${value.id}">${value.name}</option>`);                  
 
-	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+                  $('#addDepartment').append(`<option value="${value.id}">${value.name}</option>`);                  
+                  $('#departmentEdit').append(`<option value="${value.id}">${value.name}</option>`);    
+                  
+                  
+                               
+      })
+      $('#departData').html(deptData);
+    },
+    error: function (jqXHR) {
+      console.log(jqXHR);
+    }
+  })
+}
 
-	if (mysqli_connect_errno()) {
-		
-		$output['status']['code'] = "300";
-		$output['status']['name'] = "failure";
-		$output['status']['description'] = "database unavailable";
-		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-		$output['data'] = [];
-		
-		mysqli_close($conn);
+// function countPersonnel() {
 
-		echo json_encode($output);
-		
-		exit;
+//   let deptId = $(this).closest('tr').data('department-id');
 
-	}	
+//   $.ajax({
+//     url: "libs/php/countPersonnel.php",
+//     type: 'GET',
+//     data: {
+//       deptId: deptId
+//     },
+//     success: function (result) {
+//       console.log(result);
+//       // $.each(result.data, function (index, value) {
+//       //   console.log(value.count);
 
-	// SQL statement accepts parameters and so is prepared to avoid SQL injection.
-	// $_REQUEST used for development / debugging. Remember to change to $_POST for production
+//       //   $('#numPersonnel').html(value.count);
+                
+//       // })
+//     },
+//     error: function (jqXHR) {
+//       console.log(jqXHR);
+//     }
+//   })  
+// }
 
-	$query = $conn->prepare('SELECT SUM(departmentID) FROM personnel WHERE departmentID = "?"');
-	$query->bind_param("i", $_REQUEST['deptId']);
+// function countLocations() {
+//   $.ajax({
+//     url: "libs/php/countLocation.php",
+//     type: 'GET',
+//     data: {
+//       locationId: value.locationID
+//     },
+//     success: function (result) {
+  
+//       console.log(result);
+//       // $.each(result.data, function (index, value) {
+//       //   console.log(value.name);
+//       //   $('#deptLocation').html(value.name);
+//       // })    
+//     },
+//     error: function (jqXHR) {
+//       console.log(jqXHR);
+//     }
+//   })
+// }
 
-	$query->execute();
-	
-	if (false === $query) {
+  // Edit Employee
+  $("#editEmployeeSubmit").click(function(){
+    $.ajax({
+      url:"libs/php/editEmployee.php",
+      type: "POST",
+      dataType: "JSON",
+      data:{
+        id: $(`td:contains("${$('#firstNameEdit').val()}")`).closest('tr').data('personnel-id'),
+              firstName:$('#firstNameEdit').val(),
+              lastName:$('#lastNameEdit').val(),
+              jobTitle:$('#jobEdit').val(),
+              email:$('#emailEdit').val(),
+              deptId:$("#departmentEdit").val()
+      },
+      success: function(result){
+        console.log(result);
+        // $('#editEmployeeModal').modal('hide');
+        Toast.fire({
+          icon: 'success',
+          title: 'Successfully edited an employee'
+        })       
+        // getAll();  
+        $("#user_data").html('');
+        getAll(1.5);
+        myModal.toggle()
+ 
+      },
+      error:function(jqXHR){
+        console.log(jqXHR);
+      }
+      
+    });
+    
+  });
+ 
+  // Add employee
+  $("#addEmployeeSubmit").click(function(){
+    // console.log($('#addFirstName').val());
+    // console.log($("#addLastName").val());
+    // console.log($("#addJobTitle").val());
+    // console.log($("#addEmail").val());
+    // console.log($("#addDepartment").val());
+    $.ajax({
+      url:"libs/php/insertEmployee.php",
+      type: "POST",
+      dataType: "JSON",
+      data:{
+              firstName:$('#addFirstName').val(),
+              lastName:$('#addLastName').val(),
+              jobTitle:$('#addJobTitle').val(),
+              email:$('#addEmail').val(),              
+              deptId:$("#addDepartment").val()
+      },
+      success: function(result){
+        console.log(result);
+        // $('#addEmployeeModal').modal('hide');
+        Toast.fire({
+          icon: 'success',
+          title: 'Successfully added an employee'
+        })
+        // getAll();
+        $("#user_data").html('');
+        getAll(1.5);
+        myModalAdd.toggle()
+      },
+      error:function(jqXHR){
+        console.log(jqXHR);
+      }
+    });
+  });
+});
 
-		$output['status']['code'] = "400";
-		$output['status']['name'] = "executed";
-		$output['status']['description'] = "query failed";	
-		$output['data'] = [];
 
-		echo json_encode($output); 
-	
-		mysqli_close($conn);
-		exit;
-
-	}
-
-	$result = $query->get_result();
-
-   	$data = [];
-
-	while ($row = mysqli_fetch_assoc($result)) {
-
-		array_push($data, $row);
-
-	}
-
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $data;
-
-	echo json_encode($output); 
-
-	mysqli_close($conn);
-
-?>
+// Sweetalert toast initialize
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+});
