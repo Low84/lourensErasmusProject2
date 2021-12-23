3235221727a96468787198ce81656079d1744847
Lourens Erasmus <63657914+Low84@users.noreply.github.com>
2021-12-23 16:13:33 +0000

    Add files via upload

--- libs/php/countEmployees.php
+++ libs/php/countEmployees.php
@@ -35,8 +35,8 @@
 	// SQL statement accepts parameters and so is prepared to avoid SQL injection.
 	// $_REQUEST used for development / debugging. Remember to change to $_POST for production
 
-	$query = $conn->prepare('SELECT COUNT(locationID) FROM department WHERE locationID = "?"');
-	$query->bind_param("i", $_REQUEST['id']);
+	$query = $conn->prepare('SELECT COUNT(id) FROM personnel WHERE departmentID = "?"');
+	$query->bind_param("i", $_REQUEST['deptId']);
 
 	$query->execute();
 	
